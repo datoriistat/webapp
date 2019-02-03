5b739cad66fd123e9e6e56abbe3650a7a42c57ad
Emil Burzo <contact@emilburzo.com>
2019-02-03 20:28:19 +0200

    replace running Docker container on deploy

--- jenkins.sh
+++ jenkins.sh
@@ -5,4 +5,5 @@ set -eux
 mvn clean package
 
 docker build -t datoriistat .
-docker run -d --restart=always -p 6841:8080 datoriistat
+docker stop datoriistat
+docker run -d --restart=always -p 6841:8080 --name datoriistat datoriistat
